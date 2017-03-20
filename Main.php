cb49d677f499d8ad30d293431755e496a7fc4658
JadeMakie <jademakigangay@gmail.com>
2017-03-20 17:51:30 +0800

    1stclean
    waala nay ubos churva

--- Main.php
+++ Main.php
@@ -48,7 +48,7 @@ document.getElementById(".gmnoprint"){
 </script>
 
 		<!-- Google Map -->
-		    <div id="panel">
+		      <div id="panel">
       <input id="city_country" type="textbox" value="Berlin, Germany">
       <input type="button" value="Geocode" onclick="codeAddress()">
   </div>  
@@ -57,8 +57,12 @@ document.getElementById(".gmnoprint"){
     <b>Marker status:</b>
     <div id="markerStatus"><i>Click and drag the marker.</i></div>
     <b>Current position:</b>
-    <div id="info"></div>
-    <b>Closest matching a
+      <div id="info"></div>
+      <b>Closest matching address:</b>
+    <div id="address"></div>
+    </div>
+
+
 
 
 
@@ -86,41 +90,10 @@ document.getElementById(".gmnoprint"){
 				<div class="inner">
 					<h2>adVenture</h2>
 						<p>adVenture is a website that enables the users to find the right business to be having in that specific area.<br />This system is capable of suggesting and providing the best business for your needs.</p>
-				</div>
+				</div>    
 			</section>
 
 		<!-- Three -->
-			<section id="three" class="wrapper">
-				<div class="inner flex flex-3">
-					<div class="flex-item box">
-						<div class="image fit">
-							<img src="images/pic02.jpg" alt="" />
-						</div>
-						<div class="content">
-							<h3>Consequat</h3>
-							<p>Placerat ornare. Pellentesque od sed euismod in, pharetra ltricies edarcu cas consequat.</p>
-						</div>
-					</div>
-					<div class="flex-item box">
-						<div class="image fit">
-							<img src="images/pic03.jpg" alt="" />
-						</div>
-						<div class="content">
-							<h3>Adipiscing</h3>
-							<p>Morbi in sem quis dui placerat Pellentesque odio nisi, euismod pharetra lorem ipsum.</p>
-						</div>
-					</div>
-					<div class="flex-item box">
-						<div class="image fit">
-							<img src="images/pic04.jpg" alt="" />
-						</div>
-						<div class="content">
-							<h3>Malesuada</h3>
-							<p>Nam dui mi, tincidunt quis, accu an porttitor, facilisis luctus que metus vulputate sem magna.</p>
-						</div>
-					</div>
-				</div>
-			</section>
 
 			<!-- Footer -->
 			<section id="footer">
